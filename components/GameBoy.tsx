c81319e8119386d1bf53fa326ec1dc1738f7d82f
Guido Mantegna <mantegnaguido@gamil.com>
2021-11-19 10:52:32 -0300

    Added question focus

--- components/GameBoy.tsx
+++ components/GameBoy.tsx
@@ -1,28 +1,92 @@
-import { NextComponentType } from "next";
+import { NextPage } from "next";
+import React, { useState, useRef, useEffect } from "react";
 import styles from "../styles/GameBoy.module.css";
 
-const GameBoy: NextComponentType = () => {
+type Data = {
+  category: string;
+  type: string;
+  difficulty: string;
+  question: string;
+  correct_answer: string;
+  incorrect_answers: string[];
+};
+
+type Props = {
+  questions: Data[];
+};
+
+const GameBoy: NextPage<Props> = ({ questions }) => {
+  const [qNum, setQNumb] = useState<number>(0);
+  const [allAnswers, setAllAnswers] = useState<string[]>([]);
+  const [currentAnswer, setCurrentAnswer] = useState<number>(-1);
+
+  useEffect(() => {
+    setAllAnswers(() =>
+      [
+        questions[qNum].correct_answer,
+        ...questions[qNum].incorrect_answers,
+      ].sort(() => 0.5 - Math.random())
+    );
+  }, []);
+
+  const answerRef = useRef<HTMLUListElement>(null);
+
+  const handleClick = () => {
+    if (null !== answerRef.current) {
+      let allAnswers = Array.from(answerRef.current.children);
+
+      currentAnswer < allAnswers.length - 1
+        ? setCurrentAnswer(currentAnswer + 1)
+        : setCurrentAnswer(0);
+    }
+  };
+
   return (
     <div className={styles.main}>
       <div className={styles.body}>
         {/* SCREEN */}
         <div className={styles.screenBackground}>
           <div className={styles.screen}>
-            <p className={styles.screenText}>category: entertainment</p>
-            <p className={styles.screenText}>difficulty: easy</p>
-            <p className={`${styles.screenText} ${styles.question}`}>Who is the frontman of the band 30 Seconds to Mars?</p>
-            <p className={`${styles.screenText} ${styles.answer}`}>- Jared Leto</p>
-            <p className={`${styles.screenText} ${styles.answer}`}>- Gerard Way</p>
-            <p className={`${styles.screenText} ${styles.answer} ${styles.selectedAnswer}`}>- Matthew Bellamy</p>
-            <p className={`${styles.screenText} ${styles.answer}`}>- Mike Shinoda</p>
+            <p className={styles.screenText}>
+              category: {questions[qNum].category}
+            </p>
+            <p className={styles.screenText}>
+              difficulty: {questions[qNum].difficulty}
+            </p>
+            <p className={`${styles.screenText} ${styles.question}`}>
+              {questions[qNum].question}
+            </p>
+            <ul className={styles.answers} ref={answerRef}>
+              {allAnswers!.map((answer: string, index: number) => {
+                return (
+                  <li
+                    className={`${styles.screenText} ${styles.answer} ${
+                      currentAnswer === index && styles.selectedAnswer
+                    }`}
+                    key={index}
+                  >
+                    {answer}
+                  </li>
+                );
+              })}
+            </ul>
           </div>
         </div>
         {/* BUTTONS */}
         <div className={styles.buttons}>
           {/* ARROWS */}
           <div className={styles.arrowBTNS}>
-            <div className={styles.arrowsY}></div>
-            <div className={styles.arrowsX}></div>
+            <div className={styles.arrowsY}>
+              <button className={styles.arrow}></button>
+              <button
+                className={styles.arrow}
+                onClick={() => handleClick()}
+              ></button>
+            </div>
+            <div className={styles.arrowsX}>
+              <button className={styles.arrow}></button>
+              <button className={styles.arrow}></button>
+            </div>
           </div>
           {/* SELECT START AB */}
           <div className={styles.inclinedBTNS}>
@@ -32,7 +96,10 @@ const GameBoy: NextComponentType = () => {
                 <span className={styles.buttonsText}>select</span>
               </div>
               <div className={styles.startContainer}>
-                <button className={styles.startBTN}></button>
+                <button
+                  className={styles.startBTN}
+                  onClick={() => qNum < 9 && setQNumb(qNum + 1)}
+                ></button>
                 <span className={styles.buttonsText}>start</span>
               </div>
             </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,35 @@
-import type { NextPage } from 'next'
+import type { NextPage, GetStaticProps, InferGetStaticPropsType } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import GameBoy from '../components/GameBoy'
 import styles from '../styles/Home.module.css'
 
-const Home: NextPage = () => {
+type Data = {
+  category: string
+  type: string
+  difficulty: string
+  question: string
+  correct_answers: string
+  incorrect_answers: string[]
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const res = await fetch('https://opentdb.com/api.php?amount=10');
+  const data = await res.json();
+  const questions: Data[] = data.results;
+
+  return {
+    props: {
+      questions,
+    },
+  }
+}
+
+const Home: NextPage = ({ questions }: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
     <>
       <div className={styles.main}>
-        <GameBoy />
+        <GameBoy questions={questions}/>
       </div>
     </>
   )
